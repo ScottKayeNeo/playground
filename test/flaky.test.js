ebbc7facb7a8a8710f81bd8172fc953c0149006e
Scott Kaye <scott.kaye@neofinancial.com>
2024-08-02 11:08:35 -0600

    Reset

--- test/flaky.test.js
+++
@@ -1,9 +0,0 @@
-import { describe, test, expect } from "vitest";
-
-describe("Flaky test", () => {
-  test("should be flaky", () => {
-    const random = Math.random();
-
-    expect(random).toBeGreaterThan(0.8);
-  });
-});
